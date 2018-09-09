b68019d1ebc5c0be16488cf4493c06708ba7921a
Trevor Forrey <trevor4e@gmail.com>
2018-09-09 01:06:14 -0400

    updated graphql queries

--- backend/index.js
+++ backend/index.js
@@ -14,7 +14,7 @@ var schema = buildSchema(`
     }
     type Home {
         owner: User!
-        address: address!
+        address: Address!
         evacuated: Boolean!
         people_in_house: Int!
         pets_in_house: Int!
@@ -46,25 +46,37 @@ var schema = buildSchema(`
 `);
 
 const start = async () => {
+
+    // Get connections to database and collections
     const Client = await MongoClient.connect("mongodb+srv://tforrey:mongopw@evac-db-cluster-mypdv.mongodb.net/test?retryWrites=true",{ useNewUrlParser: true })
     const Homes = Client.db("EvacTrack").collection("Homes")
-    // Root resolver
+    const Users = Client.db("EvacTrack").collection("Users")
+
+    // Gets all users
+    var users = async function({owner}) {
+        return (Users.find({}).toArray())
+    }
+
+    // Toggles Evacuation status
     var evac_toggle = async function({owner}) {
         house = await Homes.findOne({owner:owner})
         await Homes.updateOne({owner:owner},{$set:{evacuated:!house.evacuated}})
         return await Homes.findOne({owner:owner})
     }
+
+    // Sets Evacuation status to true
     var set_evac_true = async function({owner}) {
         await Homes.updateOne({owner:owner},{$set:{evacuated:true}})
         return await Homes.findOne({owner:owner})
     }
+
+    // Root resolver
     var root = {
-        users: () => {
-            return (Homes.find({}).toArray())
-        },
+        users: users,
         evac_toggle: evac_toggle,
         evac: set_evac_true
     };
+
     // Create an express server and a GraphQL endpoint
     var app = express();
     app.use('/graphql', express_graphql({
