8840fe4cdf080c2ab2527e62212bff34667421b0
Paul Horton <giansar26@gmail.com>
2018-09-09 00:58:01 -0400

    Updated schema

--- backend/index.js
+++ backend/index.js
@@ -5,12 +5,29 @@ var { buildSchema } = require('graphql');
 
 // GraphQL schema
 var schema = buildSchema(`
-    type House {
-        owner: String!
+    type user {
+        name: name!
+        email: String!
+        phone_number: String!
+        home: home!
+        user_type: String!
+    }
+    type home {
+        owner: user!
         address: address!
         evacuated: Boolean!
         people_in_house: Int!
         pets_in_house: Int!
+    }
+    type evacuation{
+        date: String
+        evac_type: String
+        homes_evacuated:[home]
+    }
+
+    type name {
+        first: String!
+        last: String!
     }
     type address {
         street: String!
@@ -18,30 +35,35 @@ var schema = buildSchema(`
         state: String!
         zip: String!
     }
+
     type Query {
         users: [House]
     }
     type Mutation {
-        escape(owner:String!):House
+        evac_toggle(owner:String!):House
+        evac(owner:String!):House
     }
 `);
 
-<<<<<<< Updated upstream
 const start = async () => {
     const Client = await MongoClient.connect("mongodb+srv://tforrey:mongopw@evac-db-cluster-mypdv.mongodb.net/test?retryWrites=true",{ useNewUrlParser: true })
     const Homes = Client.db("EvacTrack").collection("Homes")
     // Root resolver
-    var toggle_escape = async function({owner}) {
+    var evac_toggle = async function({owner}) {
         house = await Homes.findOne({owner:owner})
-        console.log(house)
         await Homes.updateOne({owner:owner},{$set:{evacuated:!house.evacuated}})
+        return await Homes.findOne({owner:owner})
+    }
+    var set_evac_true = async function({owner}) {
+        await Homes.updateOne({owner:owner},{$set:{evacuated:true}})
         return await Homes.findOne({owner:owner})
     }
     var root = {
         users: () => {
             return (Homes.find({}).toArray())
         },
-        escape: toggle_escape
+        evac_toggle: evac_toggle,
+        evac: set_evac_true
     };
     // Create an express server and a GraphQL endpoint
     var app = express();
@@ -51,31 +73,6 @@ const start = async () => {
         graphiql: true
     }));
     app.listen(4000, () => console.log('Express GraphQL Server Now Running On localhost:4000/graphql'));
-=======
-var root = {
-    users: getUsers,
-    escape: toggle_escape
-};
-// Create an express server and a GraphQL endpoint
-var app = express();
-app.use('/graphql', express_graphql({
-    schema: schema,
-    rootValue: root,
-    graphiql: true
-}));
-
-app.listen(4000, () => console.log('Express GraphQL Server Now Running On localhost:4000/graphql'));
-
-
-const Client = MongoClient.connect("mongodb+srv://tforrey:mongopw@evac-db-cluster-mypdv.mongodb.net/test?retryWrites=true",{ useNewUrlParser: true })
-const Homes = Client.db("EvacTrack").collection("Homes")
-// Root resolver
-var toggle_escape = async function({owner}) {
-    house = await Homes.findOne({owner:owner})
-    console.log(house)
-    await Homes.updateOne({owner:owner},{$set:{evacuated:!house.evacuated}})
-    return await Homes.findOne({owner:owner})
->>>>>>> Stashed changes
 }
 
 start()
