fd78e7cc1db6ff637077e76f035158d9566517cf
Paul Horton <giansar26@gmail.com>
2018-09-08 21:06:30 -0400

    Maybe oneday the mutations will work

--- backend/index1.js
+++ backend/index1.js
@@ -23,17 +23,13 @@ var schema = buildSchema(`
     }
 `);
 const start = async () => {
-    const Client = await MongoClient.connect("mongodb+srv://tforrey:mongopw@evac-db-cluster-mypdv.mongodb.net/test?retryWrites=true")
-    const Homes = Client.db("EvacTrack").collection("Homes")
+    const client = await MongoClient.connect("mongodb+srv://tforrey:mongopw@evac-db-cluster-mypdv.mongodb.net/test?retryWrites=true")
+    const homes = client.db("EvacTrack").collection("Homes")
     // Root resolver
     var root = {
         users: () => {
-            return (Homes.find({}).toArray())
-        }
-        // escape: ({owner}) => {
-        //     Homes.updateOne({owner:{$eq: owner}},{$set: {evacuated: true}})
-        //     return Homes.find({owner:{$eq: owner}}).toArray()[0]
-        // }
+              return (homes.find({}).toArray())
+          }
     };
     // Create an express server and a GraphQL endpoint
     var app = express();
